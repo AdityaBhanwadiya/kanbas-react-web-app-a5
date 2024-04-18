c702bf82b0d23fd69cd51569fb18a6935028b557
aditya222001 <adityapatel020201@gmail.com>
2024-04-19 00:23:59 +0530

    a6 commit

--- src/Users/client.ts
+++ src/Users/client.ts
@@ -1,22 +1,22 @@
 import axios from "axios";
 export const BASE_API = "https://kanbas-node-server-app-me7c.onrender.com";
 export const USERS_API = `${BASE_API}/api/users`;
-export interface User {
-    _id: string; username: string; password: string; role: string;
-    firstName: string, lastName: string
-};
-export const signin = async (credentials: User) => {
-    const response = await axios.post(`${USERS_API}/signin`, credentials);
-    return response.data;
-};
+export interface User { _id: string; username: string; password: string; role: string;
+firstName: string, lastName: string };
+const api = axios.create({
+  withCredentials: true
+});
 
-export const profile = async () => {
-    const response = await axios.post(`${USERS_API}/profile`);
-    return response.data;
+export const signin = async (credentials: User) => {
+  const response = await api.post( `${USERS_API}/signin`, credentials );
+  return response.data;
 };
+export const profile = async () => {
+    const response = await api.post(`${USERS_API}/profile`);
+    return response.data;
+  };
 
 export const updateUser = async (user: any) => {
-    const response = await axios.put(`${USERS_API}/${user._id}`, user);
+    const response = await api.put(`${USERS_API}/${user._id}`, user);
     return response.data;
 };
-
